095a8ee0fc0a5f9816196b73342c49ec83ebe8c0
jinsu <realwaterkim@datacommand.kr>
2023-06-26 18:52:30 +0900

    일단 되는 중

--- main.go
+++ main.go
@@ -15,22 +15,22 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	topic := flag.String("topic", "", "topic")
-	user := flag.String("user", "", "user")
-	password := flag.String("password", "", "password")
+	//user := flag.String("user", "", "user")
+	//password := flag.String("password", "", "password")
 
 	flag.Parse()
 	if !isSet(topic) {
 		log.Println("set stream")
 		return
 	}
-	if !isSet(user) {
-		log.Println("set user")
-		return
-	}
-	if !isSet(password) {
-		log.Println("set password")
-		return
-	}
+	//if !isSet(user) {
+	//	log.Println("set user")
+	//	return
+	//}
+	//if !isSet(password) {
+	//	log.Println("set password")
+	//	return
+	//}
 
 	broker, err := mqtt.NewMqtt()
 	if err != nil {
